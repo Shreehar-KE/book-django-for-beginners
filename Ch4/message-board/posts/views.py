14e3175377ca8e8962bc10b0fca27e0108b6da67
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-06-14 22:07:59 +0530

    revise Chapter-4

--- Ch4/message-board/posts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-from django.views.generic import ListView
-
-
-class HomePageView(ListView):
-    model = Post
-    template_name = 'home.html'
